675bd019795d8f34948af48d233c5076b382b48c
Mert Alnuaimi <snickns@gmail.com>
2023-10-03 21:45:53 +0200

    smooth slide in

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,18 +1,16 @@
 document.getElementById("hamburger-icon").addEventListener("click", function() {
     var menu = document.getElementById("slide-in-menu");
-    if (menu.style.left === "0px") {
-        menu.style.left = "-100%";
+    if (menu.classList.contains('active')) {
+        menu.classList.remove('active');
     } else {
-        menu.style.left = "0px";
+        menu.classList.add('active');
     }
 });
 
-// This will close the slide-in menu if you click outside of it
 document.addEventListener("click", function(event) {
     var menu = document.getElementById("slide-in-menu");
 
-    // Check if menu is open and click is outside both the menu and the hamburger icon
-    if (menu.style.left === "0px" && !menu.contains(event.target) && !document.getElementById("hamburger-icon").contains(event.target)) {
-        menu.style.left = "-100%";
+    if (menu.classList.contains('active') && !menu.contains(event.target) && !document.getElementById("hamburger-icon").contains(event.target)) {
+        menu.classList.remove('active');
     }
 });
