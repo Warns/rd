61d79771d1d1cb4d9b43c396d8987815c221f143
Mert Alnuaimi <snickns@gmail.com>
2024-01-30 01:37:40 +0100

    Update section-head.php

--- partials/section-head.php
+++ partials/section-head.php
@@ -1,6 +1,7 @@
 <div class="section-head">
     <?php if( $args['title'] ) : ?>
-    <h2 id="<?php echo $args['slug']; ?>">
+    <!-- <h2 id="<?php echo $args['slug']; ?>"> -->
+    <h2 id="<?php echo isset($args['slug']) ? $args['slug'] : ''; ?>">
         <?php echo $args['title']; ?>
     </h2>
     <?php endif; ?>
